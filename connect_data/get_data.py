11f3e91d6e9323682ff3d43abcdb4f42003fab61
nemanja <nemanja.majstorovic3214@gmail.com>
2024-11-25 10:30:17 +0100

    Fixed NaN problem

--- connect_data/get_data.py
+++ connect_data/get_data.py
@@ -13,7 +13,7 @@ coverpage.drop(['FILINGMANAGER_STREET1', 'FILINGMANAGER_STREET2', 'FILINGMANAGER
 
 infotable_use_cols = ["ACCESSION_NUMBER", 'CUSIP', "VALUE", "SSHPRNAMT", "NAMEOFISSUER"]
 infotable = pd.read_csv("INFOTABLE.tsv", usecols=infotable_use_cols,
-                        header = 0,delimiter='\t', dtype={'ACCESSION_NUMBER': str})
+                        header = 0,delimiter='\t', dtype={'ACCESSION_NUMBER': str, "CUSIP": str})
 infotable = infotable.rename(columns={"CUSIP": "cusip", "VALUE": "value", "SSHPRNAMT": "shares", "NAMEOFISSUER": "companyName"})
 
 infotable['cusip6'] = infotable['cusip'].str[:6]
@@ -23,21 +23,38 @@ other_manager = pd.read_csv("OTHERMANAGER.tsv", usecols=other_manager_cols,
                         header = 0,delimiter='\t', dtype={'ACCESSION_NUMBER': str, 'CIK': str})
 other_manager = other_manager.rename(columns={"CIK": "managerCik"})
 
+other_manager2 = pd.read_csv("OTHERMANAGER2.tsv", usecols=other_manager_cols,
+                        header = 0,delimiter='\t', dtype={'ACCESSION_NUMBER': str, 'CIK': str})
+other_manager2 = other_manager2.rename(columns={"CIK": "managerCik"})
+
+other_manager = pd.concat([other_manager, other_manager2], ignore_index=True)
+
 other_manager['source'] = other_manager.apply(
-    lambda row: f"https://sec.gov/Archives/edgar/data/{row['managerCik']}%2F{row['ACCESSION_NUMBER']}.txt", axis=1
+    lambda row: f"https://sec.gov/Archives/edgar/data/{row['managerCik']}/{row['ACCESSION_NUMBER']}.txt", axis=1
 )
 # row = other_manager.iloc[0]  # Get the first row
 # url = f"https://sec.gov/Archives/edgar/data/{row['managerCik']}/{row['ACCESSION_NUMBER']}.txt"
 # print(url)
 # print(other_manager["source"])
 
-result = pd.merge(other_manager, infotable, on='ACCESSION_NUMBER', how='left')
-result = pd.merge(result, coverpage, on='ACCESSION_NUMBER', how='outer')
+companies_cols = ["CUSIP", "CIK"]
+companies = pd.read_csv("company_metadata.tsv", usecols=companies_cols,
+                        header = 0,delimiter='\t', dtype={'CUSIP': str, 'CIK': str})
+companies = companies.rename(columns={"CUSIP": "cusip"})
 
+result = pd.merge(companies, infotable, on='cusip', how='left')
+# result = pd.merge(result, other_manager, on='ACCESSION_NUMBER', how='left')
+result = pd.merge(result, coverpage, on='ACCESSION_NUMBER', how='left')
+# print(result["source"])
+result.to_csv('output.tsv', sep='\t', index=False, encoding='utf-8')
 
+# nan_counts = result.isna().sum()
+
+# Display columns with NaN values and their counts
+# print(nan_counts[nan_counts > 0])
+# print(result["source"])
 
 print(result.head())
-# print(result["source"])
 
 
 # matching_columns = []
